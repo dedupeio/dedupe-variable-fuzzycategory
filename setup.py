29f27c0ab18667413f2d79c72f6447338f27a7ac
Forest Gregg <fgregg@uchicago.edu>
2015-12-01 10:12:42 -0600

    not handling none

--- setup.py
+++ setup.py
@@ -7,9 +7,11 @@ except ImportError :
 setup(
     name='dedupe-variable-fuzzycategory',
     url='https://github.com/datamade/dedupe-variables-fuzzycategory',
-    version='0.0.0',
+    version='0.0.1',
     description='Fuzzy Categoy variable type for dedupe',
     packages=['dedupe.variables'],
-    install_requires=['dedupe', 'fuzzycategory', 'future'],
+    install_requires=['dedupe', 
+	              'fuzzycategory>=0.0.4', 
+                      'future'],
     license='The MIT License: http://www.opensource.org/licenses/mit-license.php'
     )
